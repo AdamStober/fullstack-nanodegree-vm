bf0b6db91c59b96617fff0e0b92fb7be08d2fbf7
Adam Stober <adamstober@adams-air.attlocal.net>
2016-07-09 13:57:29 -0700

    Revisions from Second Review
    Tournament.py: PEP8 formatting changes; protected from SQL injection
    attacks; refactored DB connection code

--- vagrant/tournament/README.md
+++ vagrant/tournament/README.md
@@ -4,15 +4,39 @@
 This program will pair players in a tournament according to a Swiss algorithm.
 Players are paired based on having the same number of wins.
 
-### Configuration:
-Install Python and PostgreSQL.
+## What's included?
+Tournament Project
+-tournament.sql
+-tournament.py
+-tournament_test.py
 
-Instructions on configuring through a vagrant virtual environment are at
+### tournament.sql
+This sets up a tournament database schema with tables like
+Players and Matches, and Views that include Wins per player,
+matches per player, and standings.
+
+### tournament.py
+This python file contains methods to connect to the tournament
+database, add and delete players, add matches, and pair players.
+
+### tournament_test.py
+This python file contains scripts that test the methods implemented in tournament.py .
+
+## Configuration:
+Install Vagrant and VirtualBox.
+Then, launch the Vagrant VM.
+
+Instructions on configuring a vagrant virtual environment are at
 https://www.udacity.com/wiki/ud197/install-vagrant
 
 ### Operating
-There is no front end to tournament.py
-Test tournament.py by running tournament_test.py on the command line
+Run a test suite to verify your code (tournament_test.py).
+To execute your tests, launch your Vagrant Virtual machine with the command "vagrant up".
+Log in to the virtual machine with the command "vagrant ssh".
+Navigate to the tournament project folder.
+Run "python tournament_test.py".
+If all tests pass, the tournament.py program is running successfully!
+
 
 #### Copyright and licensing information
 All the files are belong to me, Adam
@@ -24,8 +48,9 @@ All the files are belong to me, Adam
 Good luck!
 
 #### Credits and acknowledgements
-Thanks Udacity and Mr. Christian !
+Thanks Udacity, Udacity Andy, and Disqus Christian !
 
 ##### Changelog
 N/A: V1.0, created June 15, 2016
 V1.1, created June 22, 2016 - updated README
+V1.2, created July 7, 2016 - updated README and tournament.py
--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,69 +1,74 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
-import bleach
 
-def connect():
+
+def connect(database_name="tournament"):
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    conn = psycopg2.connect("dbname=tournament")
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("Something went wrong :-/")
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
+    db, cursor = connect()
     query = "DELETE FROM Matches;"
-    c.execute(query)
-    conn.commit() 
-    conn.close() 
+    cursor.execute(query)
+    db.commit()
+    db.close()
+
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
+    db, cursor = connect()
     query = "DELETE FROM Players;"
-    c.execute(query)
-    conn.commit() 
-    conn.close() 
+    cursor.execute(query)
+    db.commit()
+    db.close()
+
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
+    db, cursor = connect()
     query = "SELECT COUNT (*) from Players;"
-    c.execute(query)
-    result = c.fetchall()
-    #print 'res is %s\n\n' % result
-    conn.close()
-    #print 'CountPlayers result is %s\n\n' % result[0][0]
-    return result[0][0]
+    cursor.execute(query)
+    result = cursor.fetchone()
+    # print 'res is %s\n\n' % result
+    db.close()
+    # print 'CountPlayers result is %s\n\n' % result[0]
+    return result[0]
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
-    #Note: structure below lets execute() do both string substition and prevention of SQL injection
+    db, cursor = connect()
+    # Note: structure below lets execute() do both string substition
+    # and prevention of SQL injection
     query = "INSERT INTO Players (name) Values (%s);"
-    c.execute(query, (name,))
-    conn.commit() 
-    conn.close()
+    cursor.execute(query, (name,))
+    db.commit()
+    db.close()
+
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -72,14 +77,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
+    db, cursor = connect()
     query = "SELECT * FROM Standings;"
-    c.execute(query)
-    result = c.fetchall()
-    conn.commit() 
-    #print 'playerStandings res is %s\n\n' % result
-    conn.close()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    db.commit()
+    db.close()
     return result
 
 
@@ -90,22 +93,22 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
-    query = "INSERT INTO Matches (winner,loser) Values ('%s','%s');" % (winner, loser)
-    c.execute(query)
-    conn.commit() 
-    conn.close()
+    db, cursor = connect()
+    query = "INSERT INTO Matches (winner,loser) Values ('%s','%s');"
+    cursor.execute(query, (winner, loser))
+    db.commit()
+    db.close()
     return
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -113,41 +116,25 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
-    #query to get tuple:
-    query = "SELECT ID, NAME FROM Standings ORDER BY wins;"
-    c.execute(query)
-    tourneyPlayers = c.fetchall()
-    # print 'swissPairings query is %s' % tourneyPlayers
-    # print len(tourneyPlayers)
-    # print (tourneyPlayers[1] + tourneyPlayers[2])
-    # print type(tourneyPlayers)
-    # print type(Pairings(tourneyPlayers))
+    db, cursor = connect()
+    # query to get tuple:
+    query = "SELECT ID, NAME FROM Standings;"
+    cursor.execute(query)
+    tourneyPlayers = cursor.fetchall()
+    db.close()
     return Pairings(tourneyPlayers)
 
+
 def Pairings(playersAndScores):
-#players and scores go in, players in pairs come out
+    """
+    Takes players and scores, returns player IDs and names
+
+        Args:
+            playersAndScores: Player IDs and scores from Standings
+        Returns:
+            Pairs of Players, including Player IDs and names
+    """
     pairs = []
-    for i in range(0, len(playersAndScores),2):
+    for i in range(0, len(playersAndScores), 2):
         pairs.append((playersAndScores[i] + playersAndScores[i+1]))
-   #alternative method
-    # p1 = playersAndScores[i]
-    # p2 = playersAndScores[i+1]
-    # p1_id = p1[0]
-    # p1_name = p1[1]
-    # p2_id = p2[0]
-    # p2_name = p2[1]
-    # pairs.append((p1_id, p1_name, p2_id, p2_name))
-    #print pairs
     return pairs
-
-
-# advanced fancy-pants Christian's way
-# def PairWise(seq):
-#     seq = iter(seq)
-#     while seq:
-#         pair = next(seq)+next(seq)
-#         print pair
-#         yield pair
-
